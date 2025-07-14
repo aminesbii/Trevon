6a00e5ea9ff6872e38a903c40a2f9d4f188bf475
amine <aminesbii2@gmail.com>
2025-07-14 04:28:25 +0200

    setup the auth interfaces

---
+++ app/(auth)/signUp.tsx
@@ -0,0 +1,63 @@
+import { Link, router } from 'expo-router'
+import React, { useState } from 'react'
+import { Alert, Button, Text, View } from 'react-native'
+import CustomInput from '@/components/CustomInput'
+import CustomButton from '@/components/CustomButton'
+
+const signUp = () => {
+
+  const [isSubmitting , setIsSubmiting] = useState(false);
+  const [form, setForm] = useState( {name: '' , email: '' , password: ''});
+
+  const submit = async ()=> {
+    if(!form.name || !form.email || !form.password ) return Alert.alert('Error' , 'Please enter valid email adress and password.')
+      setIsSubmiting(true)
+
+    try{
+
+      Alert.alert('Success' , 'user signed in successfuly.')
+      router.replace('/');
+    } catch (error:any){
+      Alert.alert('Error', error.message )
+    } finally{
+      setIsSubmiting(false);
+    }
+  }
+
+  return (
+    <View className=" gap-10 bg-white rounded-lg p-5  mt-5">
+    <CustomInput 
+        placeholder="Enter your full name" 
+        value={form.email}
+        onChangeText={(text) => setForm( (prev) => ({... prev, email : text }))}
+        label="Full name"
+      />
+     <CustomInput 
+        placeholder="Enter your email" 
+        value={form.email}
+        onChangeText={(text) => setForm( (prev) => ({... prev, email : text }))}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput 
+        placeholder="Enter your password" 
+        value={form.password}
+        onChangeText={(text) => setForm( (prev) => ({... prev, password : text }))}
+        label="Password"
+        secureTextEntry= {true}
+        />
+      <CustomButton
+      title="log IN"
+      isLoading={isSubmitting}
+      onPress={submit}
+      /> 
+
+      <View className='flex justify-center mt-5 flex-row gap-2'>
+         <Text className="base-regular text-gray-100 ">You have an account?</Text>
+         <Link href="/signUp" className="base-bold text-primary">Sign In</Link>
+      </View> 
+    </View>
+  )
+}
+
+export default signUp
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwritecConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID ,
+    platform: "com.amine.trevon",
+    databaseId: '687455270026f3ff40a4',
+    userCollectioId: '68745586001a62b3c856'
+}
